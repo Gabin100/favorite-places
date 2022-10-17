7e5e87f691c847e941139dc5cca478055c471fa6
Jean Gabin Ishimwe <ij.gabin123@gmail.com>
2022-10-17 12:53:44 +0200

    adding navigation

---
+++ components/Places/PlaceForm.js
@@ -0,0 +1,13 @@
+import { View, Text, StyleSheet } from "react-native";
+
+function PlaceForm() {
+  return (
+    <View>
+      <Text>Place Form</Text>
+    </View>
+  );
+}
+
+export default PlaceForm;
+
+const styles = StyleSheet.create({});
