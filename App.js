55c16359d3398bd9e386b2bfa17cc8deb4228469
Jean Gabin Ishimwe <ij.gabin123@gmail.com>
2022-10-18 13:19:54 +0200

    SQLite: Getting Stated and Initialization

--- App.js
+++ App.js
@@ -1,16 +1,34 @@
+import { useEffect, useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import AppLoading from "expo-app-loading";
 
 import AllPlaces from "./screens/AllPlaces";
 import AddPlace from "./screens/AddPlace";
 import Map from "./screens/Map";
 import IconButtons from "./components/Ui/IconButtons";
 import { Colors } from "./constants/colors";
+import { initAsync } from "./util/database";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [dbInitialized, setDbInitialized] = useState(false);
+  useEffect(() => {
+    initAsync()
+      .then(() => {
+        setDbInitialized(true);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  if (!dbInitialized) {
+    return <AppLoading />;
+  }
+
   return (
     <>
       <StatusBar style='auto' />
